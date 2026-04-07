05d72f5cc47ea4426ef5fd68c671dd9625371a83
ashin333-cloud <ashinshaji333@gmail.com>
2026-04-07 11:13:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ def judge_node(state: AgentState):
 
     response = client.models.generate_content(
 
-        model="gemini-3-flash-preview", 
+        model="gemini-2.5-flash-lite", 
 
         contents=[*state['media_handles'], eval_prompt]
 
